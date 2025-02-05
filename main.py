8b96b65c60f3c1ee784b350884d86554ecb505ac
ThomasCoutarel <thomascout@hotmail.com>
2025-02-05 16:33:34 +0100

    code changé

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
 def main():
-    print('Hello, World!')
-    return 0
-
+    for i in range (0, 101):
+        if i%5==0 and i%3==0 :
+            print("fizzbuzz")
+        if i%3==0:
+            print("fizz")
+        elif i%5==0:
+            print("buzz")
+        else:
+            print(i)
 if __name__ == '__main__':
     main()
 
